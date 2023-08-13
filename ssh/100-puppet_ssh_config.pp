5ae4fb797bdd3802b0c3bf0e178e90acd890f4cc
Anissa7 <a.ouedraogo@alustudent.com>
2023-08-12 19:04:24 -0700

    the file is filing

---
+++ ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# We use puppet to make changes in our configuration file
+
+file { 'etc/ssh/ssh_cofig':
+       ensure =>present
+content =>"
+
+       #SSH client configuration
+       host*
+       Identityfile ~/.ssh/school
+       PasswordAuthentication no
+
+} 
